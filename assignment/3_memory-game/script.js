c7502942c28ac670e91971eb6d8e3dd5ab502c4a
Eric Friday <ericfriday@gmail.com>
2017-08-28 20:39:58 -0500

    Adding Exercise 3 to homework

--- assignment/3_memory-game/script.js
+++ assignment/3_memory-game/script.js
@@ -1,10 +1,17 @@
+/* Millie/Dev (whoever ends up grading this). Please note that I
+   didn't get this specific exercise done by Sunday. I'd still
+   appreciate feedback on the code if possible, but I wanted you
+   to know I missed the deadline on memory-game for grading reasons.
+   */
+
 console.log('Test Sourced');
 
 var onReady2 = function() {
   console.log('memory game doc ready');
 
   //TODO Add your code below to attach your event listeners to functions
-  
+  $('.cardDiv').on('click', singleClickFunc);
+  $('#revealHide').on('click', revealHide);
 };
 
 // on document ready run the onReady2 function
@@ -12,14 +19,21 @@ $(document).ready(onReady2);
 
 // revealHide function hides and shows all cards
 function revealHide() {
-
   //TODO add your code here to get the desired functionality
+  //This doesn't work --> $('.cardImg').toggle();
+if ($('.cardImg').is(":visible")) {
+  $('.cardImg').hide();
+}
+  else {
+    $('.cardImg').toggle();
+  }
 
+  console.log('Running revealHide()')
 }
 
 // singleClickFunc function hides and shows an indivdual card
 function singleClickFunc() {
-
   //TODO add your code here to get the desired functionality
-
+  $('.cardImg', this).toggle();
+  console.log('Running singleClickFunc()')
 }
